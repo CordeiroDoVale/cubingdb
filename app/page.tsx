9127da9d7ba2df4a2a59529a4e40ed0ef1862f79
cordeirodovale <lucascordeirodovalemenezes@gmail.com>
2023-09-03 07:30:27 -0300

    Coming Soon screen

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,12 @@ import Feed from "../components/Feed";
 export default function Home() {
   return (
     <>
-      CubingDB.com
-      <main>
-        <Feed />
+      <main className="h-screen flex flex-col items-center justify-center space-y-4 ">
+        <h1 className=" text-6xl font-mono font-bold text-stone-800">
+          {" "}
+          CubingDB.com{" "}
+        </h1>
+        <p className="font-mono text-lg">Coming Soon</p>
       </main>
     </>
   );
