33292e0bc118dcad7279c6c2adaf6db169bea5a6
cordeirodovale <lucascordeirodovalemenezes@gmail.com>
2023-09-03 06:15:50 -0300

    Project setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Feed from "../components/Feed";
+
+export default function Home() {
+  return (
+    <>
+      CubingDB.com
+      <main>
+        <Feed />
+      </main>
+    </>
+  );
+}
---
+++ components/Feed.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Feed() {
+  return (
+    <div>
+      Feed
+      <div>Selecionar puzzle blablabla</div>
+    </div>
+  );
+}
+
+export default Feed;
